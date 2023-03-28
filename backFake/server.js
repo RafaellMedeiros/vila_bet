cf7d524f3ce67701010bfa5875aec6bff65e5e1c
Rafael Medeiros <rafaelfufcg@gmail.com>
2023-03-27 22:06:32 -0300

    adicionando rota

--- backFake/server.js
+++ backFake/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const pessoa = {
     email: "use@gmail",
@@ -10,16 +11,20 @@ const pessoa = {
 
 
 app.get("/", (req, res) => {
+    res.send("Bem Vindo");
+});
 
+app.get("/teste", (req, res) => {
+    res.send(`Bem Vindo ${req.body.name}`);
 });
 
 app.post("/login", (req, res) => {
     const {email, pass} = req.body;
     if (email != pessoa.email && pass != pessoa.pass) {
-        res.send("conectado")
+        res.send("conectado");
     }
 });
 
 app.listen(5000, () => {
-    console.log("Serven is On");
+    console.log("Serven Open in PORT 5000");
 });
